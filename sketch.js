4859b34d568082899f2f519d0f9fc055b9f8e27f
pradyumnavbhat <71066128+pradyumnavbhat@users.noreply.github.com>
2020-12-15 15:52:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var canvos,backgroundImage,database;
+
+var gameState=0 ;
+var playerCount;
+var form,player,game;
+
+
+
+
+function setup(){
+  database = firebase.database();
+  
+  canvas=createCanvas(400,400);
+game= new Game();
+game.getState();
+game.start();
+}
+
+function draw(){
+  background("white");
+  
+
+
+  
+    
+}
+
+ 
+
+
